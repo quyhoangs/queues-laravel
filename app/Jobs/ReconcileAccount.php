f47163d4860d1c2983281189b646d485501edf6b
Quý Hoàng <quyhx@mor.com.vn>
2022-08-07 16:34:10 +0700

    Fail Job

--- app/Jobs/ReconcileAccount.php
+++ app/Jobs/ReconcileAccount.php
@@ -5,7 +5,6 @@ namespace App\Jobs;
 use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -22,10 +21,8 @@ class ReconcileAccount implements ShouldQueue
         $this->user = $user;
     }
 
-    public function handle(Filesystem $filesystem)
+    public function handle()
     {
-        $filesystem->put(public_path('testing.txt'),'reconciling'.$this->user->name);
-
         logger('Đang Đối chiếu tài khoản của '.$this->user->name );
     }
 }
